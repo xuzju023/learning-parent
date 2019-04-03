8c42981f800aa1edf1406b3af6f635215034f443
xuzju023 <38324922+xuzju023@users.noreply.github.com>
2019-04-03 23:54:05 +0800

    init

---
+++ lambda/src/main/java/thread/model/threadPool/CachedThreadPool.java
@@ -0,0 +1,15 @@
+package thread.model.threadPool;
+
+import thread.model.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService service= Executors.newCachedThreadPool();
+        for (int i = 0; i <5 ; i++) {
+            service.execute(new LiftOff());
+        }
+    }
+}
