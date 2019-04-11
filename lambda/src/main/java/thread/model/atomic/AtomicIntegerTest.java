c7b480b7fabbe906bacf9aec991ab26e122e455c
xuzju023 <651879800@qq.com>
2019-04-11 11:08:33 +0800

    lambda:atomicInteger

--- lambda/src/main/java/thread/model/atomic/AtomicIntegerTest.java
+++ lambda/src/main/java/thread/model/atomic/AtomicIntegerTest.java
@@ -4,49 +4,52 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: XuZhiJun
  * @Description:
  * @Date: Created in 9:44 2019/4/11
  */
-public class AtomicIntegerTest extends Thread{
+public class AtomicIntegerTest extends Thread {
     private  int i=0;
-    //private AtomicInteger i=new AtomicInteger(0);
-    public int getValue(){
+    //private AtomicInteger i = new AtomicInteger(0);
+
+    public  int getValue() {
         return i;
         //return i.get();
     }
-    void increment(){
+
+    public synchronized void increment() {
         i++;
-        i++;
-        //i.getAndIncrement(2);
+        //i.getAndIncrement();
     }
 
     @Override
     public void run() {
-        while(true){
+        for (int j = 0; j < 10000; j++) {
             increment();
         }
     }
 
-    public static void main(String[] args) {
-        new Timer().schedule(new TimerTask() {
+    public static void main(String[] args) throws InterruptedException {
+       /* new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 System.exit(0);
             }
-        }, 5000);
+        }, 5000);*/
 
         ExecutorService service = Executors.newCachedThreadPool();
         AtomicIntegerTest instance = new AtomicIntegerTest();
-        service.execute(instance);
-        while (true){
-            int next =instance.getValue();
-            if(next%2!=0){
-                System.out.println(next);
-            }
+        for (int i = 0; i <10; i++) {
+            service.execute(instance);
         }
+        Thread.sleep(1000);
+        Thread.sleep(1000);
+        Thread.sleep(1000);
+        System.out.println(instance.getValue());
+
     }
 
 }
