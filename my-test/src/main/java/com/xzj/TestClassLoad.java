2cc361ab8b84783ad5a34879ef836af76a18658e
xuzju023 <651879800@qq.com>
2019-06-25 14:07:07 +0800

    something

--- my-test/src/main/java/com/xzj/TestClassLoad.java
+++
@@ -1,9 +0,0 @@
-package com.xzj;
-
-
-public class TestClassLoad {
-    @Override
-    public String toString() {
-        return "类加载成功。";
-    }
-}
--- service-provider/src/main/java/com/xzj/learning/LerningApplication.java
+++ service-provider/src/main/java/com/xzj/learning/LerningApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class LerningApplication {
 
 	public static void main(String[] args) {
