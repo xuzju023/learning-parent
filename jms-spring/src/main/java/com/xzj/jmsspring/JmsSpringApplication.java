972cc377f89894f89ccf0ac36e1fc0546c4d46c1
xuzju023 <651879800@qq.com>
2019-10-28 20:41:05 +0800

    jms-spring

---
+++ jms-spring/src/main/java/com/xzj/jmsspring/JmsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.xzj.jmsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsSpringApplication.class, args);
+    }
+
+}
