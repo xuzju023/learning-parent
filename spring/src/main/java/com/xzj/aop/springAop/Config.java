7b2ef678d995868cfcfa5a4ff5b0b887a21d74d2
xuzju023 <38324922+xuzju023@users.noreply.github.com>
2020-06-02 23:09:39 +0800

    b3

--- spring/src/main/java/com/xzj/aop/springAop/Config.java
+++ spring/src/main/java/com/xzj/aop/springAop/Config.java
@@ -3,7 +3,7 @@ package com.xzj.aop.springAop;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
+//
 @Configuration
 @ComponentScan(basePackageClasses = {IBuy.class})
 @EnableAspectJAutoProxy(proxyTargetClass = true)
