1f937cd224a4170c31f3c026fbe0c5bfa4527d13
xuzju023 <38324922+xuzju023@users.noreply.github.com>
2020-06-02 23:28:02 +0800

    3

--- spring/src/main/java/com/xzj/aop/springAop/Test.java
+++ spring/src/main/java/com/xzj/aop/springAop/Test.java
@@ -1,7 +1,7 @@
 package com.xzj.aop.springAop;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+//3
 public class Test {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
