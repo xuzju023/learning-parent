4138ce7b69f0a584f65a4a2449d6e54950147ae6
xuzju023 <38324922+xuzju023@users.noreply.github.com>
2020-06-02 23:09:14 +0800

    b2

--- spring/src/main/java/com/xzj/aop/springAop/BuyAspectJ.java
+++ spring/src/main/java/com/xzj/aop/springAop/BuyAspectJ.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-
+//
 @Aspect
 @Component
 public class BuyAspectJ {
